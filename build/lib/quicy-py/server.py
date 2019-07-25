133330451211d1e93bf3fb923d52b7301c39679f
Lazaro Herrera <lazherrera@gmail.com>
2019-07-24 23:48:19 -0400

    change it to quicy

--- build/lib/quicy-py/server.py
+++
@@ -1,8 +0,0 @@
-from socket import socket, AF_INET, SOCK_DGRAM
-
-
-class QuicServer:
-    def __init__(self, ip, port):
-        self._connection = socket(AF_INET, SOCK_DGRAM)
-        self._connection.bind((ip, port))
-        print('[*] Server listening at {}:{}'.format(ip, port))
--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(
-    name="quicy-py",
+    name="quicy",
     keywords="python quic asyncio protocol",
     extras_require={'uvloop': ['uvloop']},
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    packages=['quicy-py', ],
+    packages=['quicy', ],
 )
