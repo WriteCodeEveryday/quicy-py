1c077ec050ba204c34cee2ad647dff0208ca6fe2
Lazaro Herrera <lazherrera@gmail.com>
2019-07-24 23:42:33 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# Sample setup.py
+
+from setuptools import setup
+
+setup(
+    name="quicy-py",
+    keywords="python quic asyncio protocol",
+    extras_require={'uvloop': ['uvloop']},
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    packages=['quicy-py', ],
+)
